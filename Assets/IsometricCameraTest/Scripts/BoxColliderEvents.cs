97d6139a7462bbced381b7105367b0c46a7bd48e
Charles Brindle <ash@blueashes.com>
2018-12-10 13:08:46 -0700

    Sexy isometric camera movement action

---
+++ Assets/IsometricCameraTest/Scripts/BoxColliderEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CleverCrow.IsometricCameras {
+    [RequireComponent(typeof(BoxCollider))]
+    public class BoxColliderEvents : MonoBehaviour {
+        public UnityEvent onEnter;
+        public UnityEvent onExit;
+        
+        private void OnTriggerEnter (Collider other) {
+            Debug.Log(other.gameObject);
+            if (!other.CompareTag("Player")) return;
+            
+            onEnter.Invoke();
+        }
+
+        private void OnTriggerExit (Collider other) {
+            if (!other.CompareTag("Player")) return;
+
+            onExit.Invoke();
+        }
+    }
+}
